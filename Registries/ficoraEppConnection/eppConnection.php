1d5d747c6aa796179401498c8e37e700f716c11d
Niko Granö <niko@ironlions.fi>
2020-09-01 14:14:40 +0300

    Support Ficora

--- Registries/ficoraEppConnection/eppConnection.php
+++ Registries/ficoraEppConnection/eppConnection.php
@@ -6,15 +6,15 @@ class ficoraEppConnection extends eppConnection {
     public function __construct($logging = false, $settingsfile = null) {
         // Construct the EPP connection object en specify if you want logging on or off
         parent::__construct($logging, $settingsfile);
-        
+
         // Specify timeout values in seconds, test interface is kind of slow
         parent::setTimeout(200);
 
         // Enable DNSSEC, Ficora supports this
         parent::enableDnssec();
 
-        // Make sure services are not added to the main EPP commands
-        parent::setServices(null);
+        // Make sure services are added
+        parent::setServices([]);
 
         // Add the commands and responses specific to this registry
         // Please make sure the corresponding PHP files are present!
